78e92116ed27745bde3429926f31ae9044b1496e
sunorry <sunorry@gmail.com>
2018-01-18 09:40:35 +0800

    INIT

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    'exports': [
+        './src/index.js',
+    ],
+    'alias': {
+        '@': 'src'
+    },
+    sourcePath: '//fe.benmu-health.com' // 默认你的资源放在 fe.benmu-health.com/vue-tpl/dist/js|css
+};
